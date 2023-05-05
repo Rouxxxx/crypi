8a7a99995dc9688650a1217c8717f1d91ad78dda
Paul FRANCOIS-MARSAL <paul.francois-marsal@epita.fr>
2023-05-05 16:11:01 +0200

    enter descriptive title

--- src/webserver/login.cpp
+++ src/webserver/login.cpp
@@ -5,15 +5,22 @@ LoginApplication::LoginApplication(const Wt::WEnvironment& env)
     , usernameEdit_(nullptr)
     , passwordEdit_(nullptr)
 {
-    setTitle("To proceed with the vote, please authentificate yourself.");
+    setTitle("Secure voting");
+
+    // Descriptive header
+    root()->addWidget(std::make_unique<Wt::WText>("Enter your credentials to proceed."));
+    root()->addWidget(std::make_unique<Wt::WBreak>());
+    root()->addWidget(std::make_unique<Wt::WBreak>());
 
     // Login text area
-    root()->addWidget(std::make_unique<Wt::WText>("Username"));
+    root()->addWidget(std::make_unique<Wt::WText>("Username "));
     usernameEdit_ = root()->addWidget(std::make_unique<Wt::WLineEdit>());
 
-    // Password text area
-    root()->addWidget(std::make_unique<Wt::WText>("Password"));
-    passwordEdit_ = root()->addWidget(std::make_unique<Wt::WLineEdit>());
-
+    // Newline
     root()->addWidget(std::make_unique<Wt::WBreak>());
+    root()->addWidget(std::make_unique<Wt::WBreak>());
+
+    // Password text area
+    root()->addWidget(std::make_unique<Wt::WText>("Password "));
+    passwordEdit_ = root()->addWidget(std::make_unique<Wt::WLineEdit>());
 }
